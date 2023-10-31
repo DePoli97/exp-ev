a4518f70416d461dcbac30b64f91927d3bb44c87
Gianluca <gianluca.maragliano@usi.ch>
2023-10-31 11:56:48 +0100

    sijfaoieng

--- assignment1/tester.java
+++ assignment1/tester.java
@@ -1,5 +1,24 @@
 package exp01;
 
+import java.util.Random;
+
 public class tester {
-    BubbleSortUntilNoChange<int> bubbleSortUntilNoChange()
+    public static void main (String[] args) {
+        Random random = new Random();
+
+        // Define the size of the array
+        int arraySize = 10000;
+
+        // Create an Integer array
+        Integer[] randomArray = new Integer[arraySize];
+
+        // Fill the array with random integers
+        for(int i = 0; i < arraySize; i++) {
+            randomArray[i] = random.nextInt(); // Generates a random integer
+        }
+
+        BubbleSortUntilNoChange<Integer> noChangeInt = new BubbleSortUntilNoChange<>();
+
+        noChangeInt.sort(randomArray);
+    }
 }
