268f888a1f7b4f138d423604302436302696eb77
Gianluca <gianluca.maragliano@usi.ch>
2023-11-14 11:11:46 +0100

    Updated Tester class for better testing

--- assignment1/Tester.java
+++ assignment1/Tester.java
@@ -11,36 +11,47 @@ import static exp01.TestCaseGenerator.generateRandomArray;
 public class Tester {
     public static void main (String[] args) {
 
+        long begin = System.currentTimeMillis();
+
         // Define the size of the array
         int arraySize = 10000;
-
-        TestCaseGenerator.SortOrder order = TestCaseGenerator.SortOrder.DESCENDING;
-
-        // Generate test case
-        Byte[] randomArray_1 = generateRandomArray(Byte.class, arraySize, order);
-        Byte[] randomArray_2 = Arrays.copyOf(randomArray_1, arraySize);
-        Byte[] randomArray_3 = Arrays.copyOf(randomArray_1, arraySize);
-
         BubbleSortUntilNoChange<Byte> noChangeInt = new BubbleSortUntilNoChange<>();
         BubbleSortPassPerItem<Byte> passInt = new BubbleSortPassPerItem<>();
         BubbleSortWhileNeeded<Byte> whileInt = new BubbleSortWhileNeeded<>();
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("sorting_results.csv", true))) {
+        TestCaseGenerator.SortOrder order = TestCaseGenerator.SortOrder.RANDOM;
+
+        System.out.println("Beginning test...");
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("byte_sorting_results.csv", true))) {
             // If the file is empty, write headers
-            if (new File("sorting_results.csv").length() == 0) {
+            if (new File("byte_sorting_results.csv").length() == 0) {
                 writer.write("Algorithm,Array Type,Array Size,Sort Order,Execution Time (ns)\n");
             }
 
-            // Perform tests and append results to the CSV file
-            testAndWriteToCSV(writer, "BubbleSortUntilNoChange", randomArray_1, noChangeInt, order);
-            testAndWriteToCSV(writer, "BubbleSortPassPerItem", randomArray_2, passInt, order);
-            testAndWriteToCSV(writer, "BubbleSortWhileNeeded", randomArray_3, whileInt, order);
+            for (int i = 0; i < 350; i++) {
+
+                // Generate test case
+                Byte[] randomArray_1 = generateRandomArray(Byte.class, arraySize, order);
+                Byte[] randomArray_2 = Arrays.copyOf(randomArray_1, arraySize);
+                Byte[] randomArray_3 = Arrays.copyOf(randomArray_1, arraySize);
+
+                // Perform tests and append results to the CSV file
+                testAndWriteToCSV(writer, "BubbleSortUntilNoChange", randomArray_1, noChangeInt, order, i);
+                testAndWriteToCSV(writer, "BubbleSortPassPerItem", randomArray_2, passInt, order, i);
+                testAndWriteToCSV(writer, "BubbleSortWhileNeeded", randomArray_3, whileInt, order, i);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        long end = System.currentTimeMillis();
+        long ex_time = (end - begin)/1000;
+        System.out.println("Completed test in: " + ex_time + " seconds");
     }
 
-    private static <T extends Comparable<T>> void testAndWriteToCSV(BufferedWriter writer, String algorithmName, T[] array, Sorter<T> sortingAlgorithm, TestCaseGenerator.SortOrder order) throws IOException {
+    private static <T extends Comparable<T>> void testAndWriteToCSV(BufferedWriter writer, String algorithmName,
+    T[] array, Sorter<T> sortingAlgorithm, TestCaseGenerator.SortOrder order, int iteration) throws IOException {
         long startTime = System.nanoTime();
         sortingAlgorithm.sort(array);
         long endTime = System.nanoTime();
@@ -50,9 +61,11 @@ public class Tester {
         String record = String.format("%s,%s,%d,%s,%d%n",
                 algorithmName, array.getClass().getSimpleName(), array.length, order.toString(), executionTime);
 
-        // Write the record to the CSV file
-        writer.write(record);
-
-        System.out.println(algorithmName + " execution time: " + executionTime + " ns");
+        // not saving first 100 cases
+        if (iteration >= 50) {
+            // Write the record to the CSV file
+            writer.write(record);
+//            System.out.println(algorithmName + " execution time: " + executionTime + " ns");
+        }
     }
 }
