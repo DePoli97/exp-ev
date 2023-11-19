e28f9b797d55d2a0fd53f759f15e619df52d8936
Gianluca <gianluca.maragliano@usi.ch>
2023-11-19 18:56:22 +0100

    Added comments

--- assignment1/src/Tester.java
+++ assignment1/src/Tester.java
@@ -13,30 +13,32 @@ public class Tester {
 
         long begin = System.currentTimeMillis();
 
-        // Define the size of the array
+        // ADJUST HERE ARRAY SIZE
         int arraySize = 10000;
+
+        // ADJUST HERE ARRAY DATA TYPE
         BubbleSortUntilNoChange<Double> noChangeInt = new BubbleSortUntilNoChange<>();
         BubbleSortPassPerItem<Double> passInt = new BubbleSortPassPerItem<>();
         BubbleSortWhileNeeded<Double> whileInt = new BubbleSortWhileNeeded<>();
 
+        // ADJUST HERE ARRAY INITIAL ORDER
         TestCaseGenerator.SortOrder order = TestCaseGenerator.SortOrder.ASCENDING;
 
         System.out.println("Beginning test...");
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("results/sorting_results.csv", true))) {
-            // If the file is empty, write headers
             if (new File("results/sorting_results.csv").length() == 0) {
                 writer.write("Algorithm,Array Type,Array Size,Sort Order,Execution Time (ns)\n");
             }
 
+            // ADJUST HERE TOTAL NUMBER OF TESTS
             for (int i = 0; i < 125; i++) {
 
-                // Generate test case
+                // ADJUST HERE ARRAY DATA TYPE
                 Double[] randomArray_1 = generateRandomArray(Double.class, arraySize, order);
                 Double[] randomArray_2 = Arrays.copyOf(randomArray_1, arraySize);
                 Double[] randomArray_3 = Arrays.copyOf(randomArray_1, arraySize);
 
-                // Perform tests and append results to the CSV file
                 testAndWriteToCSV(writer, "BubbleSortUntilNoChange", randomArray_1, noChangeInt, order, i);
                 testAndWriteToCSV(writer, "BubbleSortPassPerItem", randomArray_2, passInt, order, i);
                 testAndWriteToCSV(writer, "BubbleSortWhileNeeded", randomArray_3, whileInt, order, i);
@@ -57,15 +59,12 @@ public class Tester {
         long endTime = System.nanoTime();
         long executionTime = endTime - startTime;
 
-        // Create a CSV record
         String record = String.format("%s,%s,%d,%s,%d%n",
                 algorithmName, array.getClass().getSimpleName(), array.length, order.toString(), executionTime);
 
-        // not saving first 25 cases for warmup
+        // ADJUST HERE NUMBER OF WARM-UP ITERATIONS
         if (iteration >= 25) {
-            // Write the record to the CSV file
             writer.write(record);
-//            System.out.println(algorithmName + " execution time: " + executionTime + " ns");
         }
     }
 }
